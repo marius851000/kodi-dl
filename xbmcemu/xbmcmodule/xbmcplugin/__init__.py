3fc2f77ca01f6b6855f8017e4b9099dc656483bc
marius david <mariusdavid@laposte.net>
2020-08-28 13:17:27 +0200

    added emulation of xbmcplugin.addDirectoryItems

--- xbmcemu/xbmcmodule/xbmcplugin/__init__.py
+++ xbmcemu/xbmcmodule/xbmcplugin/__init__.py
@@ -4,6 +4,11 @@ def addDirectoryItem(handle, url, listitem, isFolder = False, totalItems = 0):
     xbmcemull.INSTANCE.get_handle(handle).add_directory_item(url, listitem, isFolder, totalItems)
     return True
 
+def addDirectoryItems(handle, items, totalItems = 0):
+    for item in items:
+        xbmcemull.INSTANCE.get_handle(handle).add_directory_item(item[0], item[1], item[2], totalItems)
+    return True
+
 def endOfDirectory(handle, succeeded = True, updateListing = False, cacheToDisc = True):
     xbmcemull.INSTANCE.get_handle(handle).end_of_directory(handle, succeeded, updateListing, cacheToDisc)
 
