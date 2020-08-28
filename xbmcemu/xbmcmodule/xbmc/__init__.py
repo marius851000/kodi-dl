1ad53b7320028337da047c7304577e725f588b58
marius david <mariusdavid@laposte.net>
2020-08-28 13:14:15 +0200

    added emulation of xbmc.log

--- xbmcemu/xbmcmodule/xbmc/__init__.py
+++ xbmcemu/xbmcmodule/xbmc/__init__.py
@@ -1,4 +1,13 @@
 import xbmcemull
 
+LOGDEBUG = 0
+LOGINFO = 1
+LOGWARNING = 2
+LOGERROR = 3
+LOGFATAL = 4
+
 def translatePath(path):
     return xbmcemull.INSTANCE.get_real_path(path)
+
+def log(msg, level=LOGDEBUG):
+    print("kodidl: log (level {}): {}".format(level, msg))
