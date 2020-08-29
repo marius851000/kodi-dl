2511b345a22b4d8d1a3f3aefeb1a5087c932c7b7
marius david <mariusdavid@laposte.net>
2020-08-29 17:18:07 +0200

    fixed emulation of addSortMethod

--- xbmcemu/xbmcmodule/xbmcplugin/__init__.py
+++ xbmcemu/xbmcmodule/xbmcplugin/__init__.py
@@ -15,10 +15,12 @@ def endOfDirectory(handle, succeeded = True, updateListing = False, cacheToDisc 
 def setResolvedUrl(handle, succeeded, listitem):
     xbmcemull.INSTANCE.get_handle(handle).set_resolved_url(succeeded, listitem)
 
-def addSortMethod(handle, sort_method):
+def addSortMethod(handle, sort_method, label2mask=0):
+    if label2mask != 0:
+        print("kodidl:todo: ignoring label2mask in addSortMethod")
     xbmcemull.INSTANCE.get_handle(handle).add_sort_method(sort_method)
 
 def setContent(handle, content):
     xbmcemull.INSTANCE.get_handle(handle).set_content(content)
-    
+
 SORT_METHOD_TRACKNUM = 7
