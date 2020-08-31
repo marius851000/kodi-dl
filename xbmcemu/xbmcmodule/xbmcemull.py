716a47a46b2a96be5c0b5750bc7a5a4a4ad2f8a5
marius david <mariusdavid@laposte.net>
2020-08-31 12:36:21 +0200

    once again fix displaying of utf-8

--- xbmcemu/xbmcmodule/xbmcemull.py
+++ xbmcemu/xbmcmodule/xbmcemull.py
@@ -1,15 +1,18 @@
 INSTANCE = None
 ADDON = None
 
+import unicodedata
+
 def to_str(thing):
     try:
         unicode
         have_unicode = True
     except:
         have_unicode = False
+
     if have_unicode:
         if type(thing) == unicode:
-            return thing.decode("utf8")
+            return unicodedata.normalize("NFKD", thing).encode("ascii", "ignore")
         else:
             return str(thing)
     else:
