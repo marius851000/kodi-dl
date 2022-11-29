b7cbff255393def2cb8eabe55270bf7c1f807a5a
marius david <marius@mariusdavid.fr>
2022-11-29 18:35:19 +0100

    add support for offscreen in ListItem

--- xbmcemu/xbmcmodule/xbmcgui/__init__.py
+++ xbmcemu/xbmcmodule/xbmcgui/__init__.py
@@ -5,7 +5,8 @@ from .dialog import Dialog
 from .window import Window
 
 class ListItem:
-    def __init__(self, label = None, path = None, label2 = None):
+    def __init__(self, label = None, path = None, label2 = None, offscreen = True):
+        self._is_instancied = True # not sure to remember why this is needed
         self.label = label
         self.label2 = label2
         self.path = path
@@ -17,27 +18,39 @@ class ListItem:
         self.context_menu_item = []
         self.x_avalaible_languages = [] #under the ietf code
         self.stream_info = {}
+        self.offscreen = offscreen # is likely to got unused
+    
+    def check_instancied(self):
+        if not hasattr(self, "_is_instancied"):
+            ListItem.__init__(self)
+
 
     def setArt(self, values):
+        self.check_instancied()
         for key in values:
             self.arts[key] = values[key]
 
     def setIconImage(self, path):
+        self.check_instancied()
         self.arts["icon"] = path
 
     def setInfo(self, type, infoLabels):
+        self.check_instancied()
         self.category = type
         for key in infoLabels:
             self.info[key] = infoLabels[key]
 
     def setProperty(self, key, value):
+        self.check_instancied()
         self.properties[key] = value
 
     def setProperties(self, dictionary):
+        self.check_instancied()
         for key in dictionary:
             self.setProperty(key, dictionary[key])
 
     def setPath(self, path):
+        self.check_instancied()
         splited_cookie = path.split("|Cookie=")
         if len(splited_cookie) > 1:
             path = splited_cookie[0]
@@ -45,28 +58,35 @@ class ListItem:
         self.path = path
 
     def getLabel(self):
+        self.check_instancied()
         return self.label
 
     def setSubtitles(self, subtitles):
+        self.check_instancied()
         self.subtitles = copy.copy(subtitles)
 
     def addContextMenuItems(self, items):
+        self.check_instancied()
         for item in items:
             self.context_menu_item.append(item)
 
     def x_addAvalaibleLanguages(self, languages):
+        self.check_instancied()
         self.x_avalaible_languages.extend(languages)
 
     def x_addAvalaibleLangueg(self, language):
+        self.check_instancied()
         self.x_avalaible_languages.extend(language)
 
     def addStreamInfo(self, category, info):
+        self.check_instancied()
         if category not in self.stream_info:
             self.stream_info[category] = {}
         for key in info:
             self.stream_info[category][key] = info[key]
 
     def pretty_print(self, pre=""):
+        self.check_instancied()
         if self.label != None:
             print(pre+"label: {}".format(xbmcemull.to_str(self.label)))
         if self.label2 != None:
@@ -90,6 +110,7 @@ class ListItem:
             print(pre+"stream info: {}".format(self.stream_info))
 
     def to_dict(self):
+        self.check_instancied()
         result = {
             "label": self.label,
             "label2": self.label2,
